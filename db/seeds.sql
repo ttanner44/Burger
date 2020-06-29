448a9703a284dab172b06548be59491791281b43
Tim Tanner <timtanner@tanner-companies.com>
2020-06-29 18:05:32 -0500

    working code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cleveland');
+INSERT INTO burgers (burger_name) VALUES ('Chi-Town');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Philly', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Austin', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo', true);
+INSERT INTO burgers (burger_name) VALUES ('Nashville');
